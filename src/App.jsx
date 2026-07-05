70c21052c2380307c57ba699f7b4fcbaa8b5f179
mohitkumar-code <mohitbhumihar98@gmail.com>
2026-07-05 18:06:17 +0530

    added song

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import LoadingScreen from "./components/sections/LoadingScreen";
 import Hero from "./components/sections/Hero";
 import Envelope from "./components/sections/Envelope";
 import WillYouBeMine from "./components/sections/WillYouBeMine";
+import song from "./assets/audio/until-i-found-you.mp3";
 
 const App = () => {
   const [isLoading, setIsLoading] = useState(true);
+  const audioRef = useRef(null);
 
-  return isLoading ? (
-    <LoadingScreen onComplete={() => setIsLoading(false)} />
-  ) : (
-    <div>
-      <Hero />
-      <Envelope />
-      <WillYouBeMine />
-    </div>
+  const handleLoadingComplete = () => {
+    setIsLoading(false);
+
+    setTimeout(() => {
+      if (audioRef.current) {
+        audioRef.current.volume = 0.8;
+        audioRef.current.play().catch((err) => console.log(err));
+      }
+    }, 300);
+  };
+
+  return (
+    <>
+      {isLoading ? (
+        <LoadingScreen onComplete={handleLoadingComplete} />
+      ) : (
+        <div>
+          <Hero />
+          <Envelope />
+          <WillYouBeMine />
+        </div>
+      )}
+
+      <audio ref={audioRef} src={song} loop />
+    </>
   );
 };
 
